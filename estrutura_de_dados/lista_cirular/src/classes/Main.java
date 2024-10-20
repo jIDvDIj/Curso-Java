744098ba4237c4d0db0bebd7586cb9f210547964
jIDvDIj <david02tk@gmail.com>
2024-10-19 23:28:54 -0300

    Finalização da lista-circular e adição da biblioteca JUnit

--- estrutura_de_dados/lista_cirular/src/classes/Main.java
+++ estrutura_de_dados/lista_cirular/src/classes/Main.java
@@ -1,29 +1,25 @@
 package classes;
 
-
-
 public class Main {
 
 	public static void main(String[] args) {
-		ListaCircular list = new ListaCircular();
-		
-		list.setList(9);
-		list.setList(2);
-		list.setList(4);
-		list.setList(5);
-		list.setList(3);
-		list.setList(2);
-		list.setList(3);
-		list.setList(6);
-		list.setList(7);
-		
-		list.printList();
-		
-		list.removeLista(7);
-		
-		list.printList();
-		
-		
-		
+		Lista list = new Lista();
+
+		list.insere(9);
+		list.insere(2);
+		list.insere(4);
+		list.insere(5);
+		list.insere(3);
+		list.insere(2);
+		list.insere(3);
+		list.insere(6);
+		list.insere(10);
+
+		System.out.println(list.imprime());
+
+		list.remove(2, false);
+
+		System.out.println(list.imprime());
+
 	}
 }
--- estrutura_de_dados/lista_cirular/src/classes/No.java
+++ estrutura_de_dados/lista_cirular/src/classes/No.java
@@ -1,29 +1,29 @@
 package classes;
 
-public class No{
-	
+public class No {
+
 	private int info;
 	private No prox;
-	
+
 	public No(int info, No prox) {
 		this.info = info;
 		this.prox = prox;
 	}
-	
+
 	public void setinfo(int info) {
 		this.info = info;
 		return;
 	}
-	
+
 	public int getInfo() {
 		return info;
 	}
-	
+
 	public void setProx(No prox) {
 		this.prox = prox;
 		return;
 	}
-	
+
 	public No getProx() {
 		return prox;
 	}
