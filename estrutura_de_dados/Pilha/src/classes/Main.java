25a04f56c08fd35a048c6ee7b960c55e8717c5b4
jIDvDIj <david02tk@gmail.com>
2024-10-24 19:54:45 -0300

    Stack implementation

---
+++ estrutura_de_dados/Pilha/src/classes/Main.java
@@ -0,0 +1,29 @@
+package classes;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Pilha pilha = new Pilha();
+		
+		pilha.push(9);
+		pilha.push(2);
+		pilha.push(4);
+		pilha.push(5);
+		pilha.push(3);
+		pilha.push(2);
+		pilha.push(3);
+		pilha.push(6);
+		pilha.push(7);
+		
+		pilha.print();
+		
+		pilha.pop();
+		
+		pilha.print();
+		
+		
+		
+	}
+}
---
+++ estrutura_de_dados/Pilha/src/classes/Pilha.java
@@ -0,0 +1,47 @@
+package classes;
+
+public class Pilha {
+	private No topo;
+
+	public Pilha() {
+		this.topo = null;
+	}
+
+	public void push(int info) {
+		No novoNo = new No(info, null);
+		if (topo == null) {
+			topo = novoNo;
+		} else {
+			novoNo.setProx(topo);
+			topo = novoNo;
+		}
+	}
+
+	public void pop() {
+		if (topo == null) {
+			System.out.println("Lista vazia");
+		} else {
+			topo = topo.getProx();
+		}
+	}
+
+	public void print() {
+		for (No aux = topo; aux!=null; aux = aux.getProx()) {
+			System.out.print(aux.getInfo() + " ");
+		}
+		System.out.println();
+	}
+	
+	public int peekPilha() {
+		return topo.getInfo();
+	}
+	
+	public void isEmptyLista() {
+		if (topo == null) {
+			System.out.println("lista vazia");
+			
+		} else {
+			System.out.println("lista não vazia");
+		}
+	}
+}
